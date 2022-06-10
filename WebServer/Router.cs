50ed8898989d7c86d23728d0893ce5d649d047cb
Li Xingjian <xingjianli59@gmail.com>
2022-06-10 17:19:39 +0800

    Add Router.cs

---
+++ WebServer/Router.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Extensions;
+
+namespace WebServer
+{
+    public class Router
+    {
+        public string WebsitePath { get; set; }
+
+        private Dictionary<string, ExtensionInfo> extFolderMap;
+
+        public Router()
+        {
+            extFolderMap = new Dictionary<string, ExtensionInfo>()
+            {
+                {"ico",  new ExtensionInfo() { Loader = ImageLoader, ContentType = "image/ico"     }},
+                {"png",  new ExtensionInfo() { Loader = ImageLoader, ContentType = "image/png"     }},
+                {"jpg",  new ExtensionInfo() { Loader = ImageLoader, ContentType = "image/jpg"     }},
+                {"gif",  new ExtensionInfo() { Loader = ImageLoader, ContentType = "image/gif"     }},
+                {"bmp",  new ExtensionInfo() { Loader = ImageLoader, ContentType = "image/bmp"     }},
+                {"html", new ExtensionInfo() { Loader = PageLoader, ContentType = "text/html"      }},
+                {"css",  new ExtensionInfo() { Loader = FileLoader, ContentType = "text/css"       }},
+                {"js",   new ExtensionInfo() { Loader = FileLoader, ContentType = "text/javascript"}},
+                {"",     new ExtensionInfo() { Loader = PageLoader, ContentType = "text/html"      }},
+            };
+        }
+
+        /// <summary>
+        /// Read in an image file, return a ResponsePacket with the raw data.
+        /// </summary>
+        private ResponsePacket ImageLoader(string fullPath, string ext, ExtensionInfo extInfo)
+        {
+            FileStream fileStream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            ResponsePacket res = new ResponsePacket()
+            {
+                Data = binaryReader.ReadBytes((int)fileStream.Length),
+                ContentType = extInfo.ContentType
+            };
+
+            binaryReader.Close();
+            fileStream.Close();
+
+            return res;
+        }
+
+        /// <summary>
+        /// Read in what is basically a text file and return a ResponsePacket with the ext UTF8 encoded.
+        /// </summary>
+        private ResponsePacket FileLoader(string fullPath, string ext, ExtensionInfo extInfo)
+        {
+            string text = File.ReadAllText(fullPath);
+            ResponsePacket res = new ResponsePacket()
+            {
+                Data = Encoding.UTF8.GetBytes(text),
+                ContentType = extInfo.ContentType, 
+                Encoding = Encoding.UTF8
+            };
+            return res;
+        }
+
+        /// <summary>
+        /// Read an HTML file, including foo.com, foo.com\index, foo.com\index.html
+        /// </summary>        
+        private ResponsePacket PageLoader(string fullPath, string ext, ExtensionInfo extInfo)
+        {
+            ResponsePacket res = new ResponsePacket();
+
+            if (fullPath == WebsitePath) // if foo.com, defaultly load index.html
+            {
+                res = Route(GET, "/index.html", null);
+            }
+            else // otherwise foo.com\index.html or foo.com\index
+            {
+                if (String.IsNullOrEmpty(ext))
+                {
+                    fullPath = fullPath + ".html";
+                }
+
+                // inject the "Page" folder into the path
+                fullPath = WebsitePath + "\\Pages" + fullPath.RightOf(WebsitePath);
+                res = FileLoader(fullPath, ext, extInfo);
+            }
+
+            return res;
+        }
+
+        public ResponsePacket Route(string httpMethod, string path, Dictionary<string, string> kvParams)
+        {
+            string ext = path.RightOf('.');
+            ExtensionInfo extInfo;
+            ResponsePacket res = null;
+
+            if (extFolderMap.TryGetValue(ext, out extInfo))
+            {
+                // '/' => '\' for windows
+                string fullPath = Path.Combine(WebsitePath, path);
+                res = extInfo.Loader(fullPath, ext, extInfo);
+            }
+
+            return res;
+
+        }
+    }
+
+    /// <summary>
+    /// Helper class: web packet 
+    /// </summary>
+    public class ResponsePacket
+    {
+        public string Redirect { get; set; }
+        public byte[] Data { get; set; }
+        public string ContentType { get; set; }
+        public Encoding Encoding { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+        public ResponsePacket()
+        {
+            //Error = Server.ServerError.OK;
+            StatusCode = HttpStatusCode.OK;
+        }
+    }
+
+    /// <summary>
+    /// File extension information: .html, .js ....
+    /// </summary>
+    internal class ExtensionInfo
+    {
+        // Delegate Func<Tn, Tr>: passing function pointers
+        public Func<string, string, ExtensionInfo, ResponsePacket> Loader { get; set; }
+        public string ContentType { get; set; }
+    }
+}
--- WebServer/Server.cs
+++ WebServer/Server.cs
@@ -1,45 +1,45 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
 
 namespace WebServer
 {
     public static class Server
     {
-        private static int maxSimultaneousConnections = 20;
-
-        // A listening socket.
-        private static HttpListener _listener;  
-
-        // A semaphore that waits for a specified number of simul-allowed connections.
-        private static Semaphore _semaphore = new Semaphore(maxSimultaneousConnections, maxSimultaneousConnections);
+        private static HttpListener _listener;
+        private static int maxSimulaneousConnections = 20;
+        private static Semaphore _semaphore = new Semaphore(maxSimulaneousConnections, maxSimulaneousConnections);
 
         /// <summary>
-        /// Return list of IP addresses assigned to localhost network devices, 
-        /// including hardwired ethernet, wireless etc.
+        /// Returns list of IP addresses assigned to localhost network devices.
         /// </summary>
         private static List<IPAddress> GetLocalHostIPs()
         {
             IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
-            List<IPAddress> res = host.AddressList.Where(
-                    ip => ip.AddressFamily == AddressFamily.InterNetwork
-                ).ToList();
-
-            // res = GetIp4Address();
-
+            List<IPAddress> res = new List<IPAddress>();
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    res.Add(ip);
+                }
+            }
             return res;
         }
 
         /// <summary>
-        /// Instantiate HttpListener and add the localhost prefixes (subnet masks).
+        /// Initialize and return a HttpListener according to localhost IPs.
         /// </summary>
         private static HttpListener InitializeListener(List<IPAddress> localhostIPs)
         {
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add("http://localhost/");
 
-            // Listen to IP address as well
+            // Listen to all IP addresses in localhost
             localhostIPs.ForEach(ip =>
             {
                 Console.WriteLine("Listening on IP " + "http://" + ip.ToString() + "/");
@@ -50,83 +50,47 @@ namespace WebServer
         }
 
         /// <summary>
-        /// Get localhost's IPv4 addresses
+        /// Internal Start() on a seperate worker thread.
         /// </summary>
-        /// <returns> List of IPv4 addresses </returns>
-        private static IEnumerable<string> GetIp4Address()
-        {
-            List<string> res = new List<string>();
-
-            foreach (IPAddress ip in Dns.GetHostAddresses(Dns.GetHostName()))
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    res.Add(ip.ToString());
-                }
-            }
-            return res;
-        }
-
-        /// <summary>
-        /// Begin listening to connections on a seperate worker thread.
-        /// </summary>
-        /// <param name="listener"> HttpListner for server start-up </param>
         private static void Start(HttpListener listener)
         {
             listener.Start();
-            Task.Run(() => RunServer(listener)); // invoke a worker thread
+            Task.Run(() => RunServer(listener));
         }
 
         /// <summary>
-        /// Start awaiting for connections, up to the "maxSimultaneousConnections" value.
-        /// Running in a seperate thread.
+        /// Start awaiting for connections, up to the maxSimultaneousConnections value.
         /// </summary>
         private static void RunServer(HttpListener listener)
         {
             while (true)
             {
-                // blocking current thread until WaitHandle recevies a signal
-                _semaphore.WaitOne(); 
+                _semaphore.WaitOne();
                 StartConnectionListener(listener);
             }
         }
 
         /// <summary>
-        /// Connection listener as an awaitable asynchronous process.
+        /// Await connections.
         /// </summary>
         private static async void StartConnectionListener(HttpListener listener)
         {
-            // wait for a connection. Return to caller while we wait.
             HttpListenerContext context = await listener.GetContextAsync();
 
-            // release the semaphore for another listener. _semaphore +1
             _semaphore.Release();
-            Log(context.Request);
-
-            // connection built now, processing
-            string response = "Connected!";
+            
+            // after connections built, give a response
+            string response = "Connection built successfully!";
             byte[] encoded = Encoding.UTF8.GetBytes(response);
             context.Response.ContentLength64 = encoded.Length;
             context.Response.OutputStream.Write(encoded, 0, encoded.Length);
             context.Response.OutputStream.Close();
         }
 
-        /// <summary>
-        /// Log requests.
-        /// </summary>
-        private static void Log(HttpListenerRequest request)
-        {
-            Console.WriteLine(request.RemoteEndPoint + " " + request.HttpMethod + " /" +
-                request.Url.AbsoluteUri.RightOf('/', 3));
-        }
-
-        /// <summary>
-        /// Starts the web server.
-        /// </summary>
         public static void Start()
         {
-            List<IPAddress> localHostIPs = GetLocalHostIPs();
-            HttpListener listener = InitializeListener(localHostIPs);
+            List<IPAddress> localhostIps = GetLocalHostIPs();
+            HttpListener listener = InitializeListener(localhostIps);
             Start(listener);
         }
     }
